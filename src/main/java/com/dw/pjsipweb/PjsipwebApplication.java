1ec16cf4f6d1c6ec418c75a3e4587f4385f52e1b
dongtinglu <2321894289@qq.com>
2021-05-18 22:19:41 +0800

    add ComponentScan

--- src/main/java/com/dw/pjsipweb/PjsipwebApplication.java
+++ src/main/java/com/dw/pjsipweb/PjsipwebApplication.java
@@ -3,9 +3,11 @@ package com.dw.pjsipweb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.dw.pjsipweb.mapper")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.dw"})
 public class PjsipwebApplication {
 
     public static void main(String[] args) {
